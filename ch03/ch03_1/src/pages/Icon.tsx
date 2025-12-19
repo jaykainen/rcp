8b423eb3c4f2b8cfcd202b716f5e780d0d2e1c30
jaykainen <jaykainen@gmail.com>
2025-12-19 15:21:25 +0900

    1219

--- ch03/ch03_1/src/pages/Icon.tsx
+++ ch03/ch03_1/src/pages/Icon.tsx
@@ -1,7 +1,17 @@
-import type {FC, DetailedHTMLProps, HTMLAttributes} from 'react'
-
-type ReactSpanProps = DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>
-
-export default function CopyMe() {
-  return <div>CopyMe</div>
+export default function Icon() {
+  return (
+    <div>
+      <h3>Icon</h3>
+      <span className="material-icons">Home</span>
+      <span className="material-icons">check_circle_outline</span>
+    </div>
+  )
 }
+
+// import type {FC, DetailedHTMLProps, HTMLAttributes} from 'react'
+
+// type ReactSpanProps = DetailedHTMLProps<HTMLAttributes<HTMLSpanElement>, HTMLSpanElement>
+
+// export default function CopyMe() {
+//   return <div>CopyMe</div>
+// }
