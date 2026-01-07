f443524e999e96dfa963439c5d052bf820bdf47e
jaykainen <jaykainen@gmail.com>
2026-01-07 17:28:16 +0900

    260107_1

--- ch04/ch04_3/src/pages/ArrayState.tsx
+++ ch04/ch04_3/src/pages/ArrayState.tsx
@@ -1,8 +1,54 @@
-export default function CopyMe() {
+import {useCallback, useState, useMemo} from 'react'
+import {Title, Div} from '../components'
+import {Icon} from '../theme/daisyui'
+import * as D from '../data'
+
+export default function ArrayState() {
+  const [images, setImages] = useState<string[]>([])
+  const addImage = useCallback(() => {
+    setImages(images => [D.randomImage(200, 100, 500), ...images])
+  }, [])
+
+  const clearImages = useCallback(() => {
+    setImages(notUsed => [])
+  }, [])
+
+  const children = useMemo(
+    () =>
+      images.map((image, index) => (
+        <Div
+          key={index}
+          className="w-1/5 m-2"
+          src={image}
+          height="5rem"
+          minHeight="5rem"
+        />
+      )),
+    [images]
+  )
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">Copy Me</h2>
-      <div className="mt-4"></div>
+      <Title>ArrayState</Title>
+      <div className="flex justify-center mt-4">
+        <div data-tip="add image" className="tooltip">
+          <Icon
+            name="add"
+            onClick={addImage}
+            className="mr-12 btn-primary"
+            iconClassName="text-3xl"
+          />
+        </div>
+        <div data-tip="clear all" className="tooltip">
+          <Icon
+            name="clear_all"
+            onClick={clearImages}
+            className="mr-12 btn-primary"
+            iconClassName="text-3xl"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap mt-4">{children}</div>
     </section>
   )
 }
--- ch04/ch04_3/src/pages/RadioInputTest.tsx
+++ ch04/ch04_3/src/pages/RadioInputTest.tsx
@@ -1,9 +1,14 @@
-import {useMemo} from 'react'
+import type {ChangeEvent} from 'react'
+import {useMemo, useCallback, useState} from 'react'
 import {Title, Subtitle} from '../components'
 import * as D from '../data'
 
 export default function RadioInputTest() {
   const jobTitles = useMemo(() => D.makeArray(4).map(D.randomJobTitle), [])
+  const [selectedJob, setSelectedJob] = useState<string>(jobTitles[0])
+  const onChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setSelectedJob(e.target.value)
+  }, [])
   const radioInputs = useMemo(
     () =>
       jobTitles.map((value, index) => (
@@ -13,18 +18,20 @@ export default function RadioInputTest() {
             name="jobs"
             defaultValue={value}
             className="mr-4 radio-primary"
+            checked={selectedJob === value}
+            onChange={onChange}
           />
           <span className="label-text">{value}</span>
         </label>
       )),
-    [jobTitles]
+    [jobTitles, selectedJob, onChange]
   )
   return (
     <section className="mt-4">
       <Title>RadioInputTest</Title>
       <div className="flex flex-col justify-center mt-4">
         <Subtitle>what's job?</Subtitle>
-        <Subtitle className="mt-4">Selected Job:</Subtitle>
+        <Subtitle className="mt-4">Selected Job:{selectedJob}</Subtitle>
         <div className="flex justify-center p-4 mt-4">
           <div className="flex flex-col mt-4">{radioInputs}</div>
         </div>
