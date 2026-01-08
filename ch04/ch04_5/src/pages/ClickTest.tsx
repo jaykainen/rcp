2e142ee9cf87576259ae481f3cebc9ddb8a6cb51
jaykainen <jaykainen@gmail.com>
2026-01-08 17:33:10 +0900

    260108_2

--- ch04/ch04_5/src/pages/ClickTest.tsx
+++ ch04/ch04_5/src/pages/ClickTest.tsx
@@ -1,8 +1,21 @@
-export default function CopyMe() {
+import {useRef, useCallback} from 'react'
+import {Title} from '../components'
+import {Button} from '../theme/daisyui'
+
+export default function ClickTest() {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const onClick = useCallback(() => inputRef.current?.click(), [])
+
   return (
     <section className="mt-4">
-      <h2 className="text-5xl font-bold text-center">Copy Me</h2>
-      <div className="mt-4"></div>
+      <Title>Click Test</Title>
+      <div className="flex items-center justify-center mt-4">
+        {/* onClick 클릭과 input 클릭이 똑같이 동작함!! */}
+        <Button className="mr-4 btn-primary" onClick={onClick}>
+          Click Me
+        </Button>
+        <input type="file" ref={inputRef} accept="image/*" />
+      </div>
     </section>
   )
 }
